f35c84a545cb14f740b8be1e54ca7e427640db0f
Andrew-Neely-82 <114946953+Andrew-Neely-82@users.noreply.github.com>
2022-11-05 23:42:44 -0500

    updated

--- Javascript/loops/for each loops/for-each-loops.js
+++ Javascript/loops/for each loops/for-each-loops.js
@@ -1,42 +1,42 @@
 (function () {
-  // for each loops
-  // for each loops are used to iterate over arrays
+	// for each loops
+	// for each loops are used to iterate over arrays
 
-  // example 1
-  const numbers = [1, 2, 3, 4, 5];
-  const letter = [`a`, `b`, `c`, `d`, `e`];
+	// example 1
+	const numbers = [1, 2, 3, 4, 5];
+	const letter = [`a`, `b`, `c`, `d`, `e`];
 
-  // output as number data type
+	// output as number data type
 
-  console.log(`Numbers as number data type:`);
+	console.log(`Numbers as number data type:`);
 
-  numbers.forEach(function (number) {
-    console.log(number);
-  });
+	numbers.forEach(function (number) {
+		console.log(number);
+	});
 
-  console.log(``);
+	console.log(``);
 
-  // numbers as string data type
+	// numbers as string data type
 
-  console.log(`Numbers as string data type:`);
+	console.log(`Numbers as string data type:`);
 
-  numbers.forEach(function (number) {
-    console.log(`${number} as string`);
-  });
+	numbers.forEach(function (number) {
+		console.log(`${number} as string`);
+	});
 
-  console.log(``);
+	console.log(``);
 
-  // es6 arrow function with for each loop
-  console.log(`es6 arrow function with for each loop:`);
+	// es6 arrow function with for each loop
+	console.log(`es6 arrow function with for each loop:`);
 
-  numbers.forEach((number) => {
-    console.log(number);
-  });
+	numbers.forEach((number) => {
+		console.log(number);
+	});
 
-  console.log(``)
-  console.log(`String data type:`)
+	console.log(``);
+	console.log(`String data type:`);
 
-  letter.forEach((letter) => {
-    console.log(letter);
-  });
+	letter.forEach((letter) => {
+		console.log(letter);
+	});
 })();
