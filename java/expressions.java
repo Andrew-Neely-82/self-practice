b9ca50df85920592102eb7ba86ca693711d3b64f
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-12-14 18:13:00 -0600

    learning Java

---
+++ java/expressions.java
@@ -0,0 +1,14 @@
+
+public class expressions {
+  public static void main(String[] args) {
+    // int friends = 10;
+    double friends = 10;
+
+    // friends = friends + 1; // adds 1 to friends
+    // friends++; // adds 1 to friends
+    friends += 1.123; // adds 1 to friends
+
+    System.out.println("");
+    System.out.println(friends);
+  }
+}
---
+++ java/gui.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class gui {
+  public static void main(String[] args){
+    String name = JOptionPane.showInputDialog("What is your name?");
+    JOptionPane.showMessageDialog(null, "Hello, " + name + "!");
+
+    int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you?"));
+    JOptionPane.showMessageDialog(null, name + " is " + age + " years old!");
+
+    double height = Double.parseDouble(JOptionPane.showInputDialog("How tall are you in inches?"));
+    JOptionPane.showMessageDialog(null, name + " is " + height + " inches tall!");
+  }
+}
