95e07240acd1a4171c0d174394481ce4e8ff3b36
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-12-16 16:42:42 -0600

    updated to remove errors

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -4,56 +4,56 @@ public class HelloWorld {
     String world = "World!";
     System.out.println(hello + world);
     
-    // TODO 1
+    // * TODOne 1
     // Create an int variable named myFavoriteNumber and assign your favorite number
     // to it, then print it out to the console.
     int myFavoriteNumber = 7;
     System.out.println(myFavoriteNumber);
 
-    // TODO 2
+    // * TODOne 2
     // Create a String variable named myString and assign a string value to it, then
     // print the variable out to the console.
     String myString = "This is a string.";
     System.out.println(myString);
 
-    // TODO 3
+    // * TODOne 3
     // Change your code to assign a character value to myString. What do you notice?
     // myString = 'c'; // does not work
 
-    // TODO 4
+    // * TODOne 4
     // Change your code to assign the value 3.14159 to myString. What happens?
     // myString = 3.14159; // does not work
 
-    // TODO 5
+    // * TODOne 5
     // Declare an long variable named myNumber, but do not assign anything to it.
     // Next try to print out myNumber to the console. What happens?
     // long myNumber;
     // System.out.println(myNumber); // does not work
 
-    // TODO 6
+    // * TODOne 6
     // Change your code to assign the value 3.14 to myNumber. What do you notice?
     // myNumber = 3.14; // does not work
     // System.out.println(myNumber);
 
-    // TODO 7
+    // * TODOne 7
     // Change your code to assign the value 123L (Note the 'L' at the end) to
     // myNumber.
     // myNumber = 123L;
     // System.out.println(myNumber);
 
-    // TODO 8
+    // * TODOne 8
     // Change your code to assign the value 123 to myNumber.
     // myNumber = 123;
     // System.out.println(myNumber);
 
-    // TODO 9
+    // * TODOne 9
     // Change your code to declare myNumber as a float. Assign the value 3.14 to it.
     // What happens? What are two ways we could fix this?
     // float myNumber; // does not work
     // myNumber = 3.14; // does not work
     // System.out.println(myNumber);
 
-    // TODO 10
+    // * TODOne 10
     // int x = 5;
     // System.out.println(x++); // 5
     // System.out.println(x); // 6
@@ -62,16 +62,16 @@ public class HelloWorld {
     // System.out.println(++x); // 6
     // System.out.println(x); // 6
 
-    // TODO 11
+    // * TODOne 11
     // Try to create a variable named class. What happens?
     // String class = "This is a string."; // does not work
 
-    // TODO 12
-    String theNumberThree = "three"; // does not work
-    Object o = theNumberThree; // does not work
-    int three = (int) o; // does not work
+    // * TODOne 12
+    // String theNumberThree = "three"; // does not work
+    // Object o = theNumberThree; // does not work
+    // int three = (int) o; // does not work
 
-    // TODO 13 
+    // * TODOne 13 
     // Rewrite the following expressions using the relevant shorthand assignment
     // operators:
 
@@ -90,7 +90,7 @@ public class HelloWorld {
     // y = y - x;
     // x /= y;
 
-    // TODO 14
+    // * TODOne 14
     // What happens if you assign a value to a numerical variable that is larger (or smaller)
     // than the type can hold? What happens if you increment a numeric variable past the
     // type's capacity?
