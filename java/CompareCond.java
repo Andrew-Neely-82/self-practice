d783f5c5a1b7ed85fdb0068991781fb98c5ac0f4
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-12-16 16:00:50 -0600

    added

---
+++ java/CompareCond.java
@@ -0,0 +1,18 @@
+public class CompareCond {
+  public static void main(String[] args) {
+
+    byte age = 18;
+
+    if (age >= 18) {
+      System.out.println("you are an adult");
+    } else if (age >= 65) {
+      System.out.println("You are a senior citizen");
+    } else {
+      System.out.println("You are not an Adult");
+    }
+  }
+}
+
+// could not find or load main class conditionals
+// how to fix this error?
+// answer is to change the file name to Conditionals.java
