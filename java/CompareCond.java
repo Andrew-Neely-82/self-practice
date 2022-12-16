045d0a1f2a4eaba4f24f01af7775286ef5dd14c1
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-12-16 16:30:40 -0600

    learning if-statements with GUI and scanner

--- java/CompareCond.java
+++ java/CompareCond.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class CompareCond {
   public static void main(String[] args) {
 
-    byte age = 17;
+    // how to do with scanner //
 
-    if (age >= 65) {
-      System.out.println("You are a senior citizen");
-    } else if (age >= 18) {
-      System.out.println("You are an adult");
-    } else if (age < 18) {
-      System.out.println("You are a minor");
+    // Scanner scanner = new Scanner(System.in);
+    // System.out.println("Enter your age: ");
+    // age = scanner.nextByte();
+
+    // how to do with JOptionPane //
+
+    String age = JOptionPane.showInputDialog("How old are you?");
+    int ageInt = Integer.parseInt(age);
+
+    if (ageInt >= 65) {
+      JOptionPane.showMessageDialog(null, "You are a Senior!");
+    } else if (ageInt >= 40) {
+      JOptionPane.showMessageDialog(null, "You are a middle-aged!");
+    } else if (ageInt >= 35) {
+      JOptionPane.showMessageDialog(null, "You are a adult!");
+    } else if (ageInt >= 18) {
+      JOptionPane.showMessageDialog(null, "You are a young adult!");
+    } else {
+      JOptionPane.showMessageDialog(null, "You are a child!");
     }
   }
 }
-
-// could not find or load main class conditionals
-// how to fix this error?
-// answer is to change the file name to Conditionals.java
