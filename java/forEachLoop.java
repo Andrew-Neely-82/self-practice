79119aa0486a1fcf25b3f28a4e6c86c3edb18abc
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-01-10 15:27:52 -0600

    added

---
+++ java/forEachLoop.java
@@ -0,0 +1,13 @@
+public class forEachLoop {
+  public static void main(String[] args) {
+
+    String[] animals = { "Dog", "Cat", "Bird", "Fish" };
+
+    for (String x : animals) {
+      System.out.println(x);
+    }
+
+
+    // the for each loop uses the data type of the array with a parameter, followed by a colon, followed by the name of the array
+  }
+}
