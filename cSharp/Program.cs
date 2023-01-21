f9769dc5a46f40256a746006ca12ee37a9411bde
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-01-20 19:58:28 -0600

    made a function to console out information only using print instead

--- cSharp/Program.cs
+++ cSharp/Program.cs
@@ -1,46 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
+static void print(string input) {
+    Console.WriteLine(input);
+}
+
 // * fizzbuzz
-System.Console.WriteLine("\n\n" + "simple fizzbuzz loop:");
+print("\n\n" + "simple fizzbuzz loop:");
 
 for (int i = 0; i <= 15; i++) {
   if (i % 3 == 0 && i % 5 == 0 && i != 0) {
-    Console.WriteLine(i + " Fizzbuzz");
+    print(i + " Fizzbuzz");
   } else if (i % 5 == 0 & i != 0) {
-    Console.WriteLine(i + " Fizz");
+    print(i + " Fizz");
   } else if (i % 3 == 0 && i != 0) {
-    Console.WriteLine(i  + " Buzz");
+    print(i  + " Buzz");
   } else {
-    Console.WriteLine(i);
+    print(i);
   }
 }
 
 // * arrays
-System.Console.WriteLine("\n\n" + "int array for loop: ");
+print("\n\n" + "int array for loop: ");
 int[] array = {1, 2, 3, 4, 5};
 
 for (int i = 0; i <= array.Length -1; i++) {
-  Console.WriteLine(array[i]);
+  print(array[i]);
 }
 
-System.Console.WriteLine("\n\n" + "looping through a names array: ");
+print("\n\n" + "looping through a names array: ");
 string[] names = {"andrew", "bob", "charles", "david", "frank"};
 
 // for (int i = 0; i < names.Length; i++) {
-//     Console.WriteLine(names[i]);
+//     print(names[i]);
 // }
 
 foreach (string a in names) {
-  Console.WriteLine(a);
+  print(a);
 }
 
-Console.WriteLine("\n\nadd function: ");
+print("\n\nadd function: ");
 
 static int add(int a, int b) {
   int result = a + b;
-  Console.WriteLine(result);
+  print(result);
   return result;
 }
 
 add(1, 2);
+
+print("test");
