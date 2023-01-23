3cfd9d514fd012608774aea87735f0445241c6b0
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-01-22 21:31:25 -0600

    jquery is finally working with typescript/ probably too many files now

--- siteRecreation/githubProfile/dist/js/app.js
+++ siteRecreation/githubProfile/dist/js/app.js
@@ -1,14 +1,11 @@
 "use strict";
-let __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const jquery_1 = __importDefault(require("jquery"));
-(0, jquery_1.default)(document).ready(() => {
-    (0, jquery_1.default)(`.git-svg`).click((e) => {
-        e.preventDefault();
-        console.log(`clicked`);
+(() => {
+    $(document).ready(() => {
+        $(`.git-svg`).click((e) => {
+            e.preventDefault();
+            console.log(`clicked`);
+        });
     });
-});
+})();
 // ? set the current dir by "cd siteRecreation/githubProfile"
 // ? to compile run => "tsc -p githubProfile"
--- siteRecreation/githubProfile/src/app.ts
+++ siteRecreation/githubProfile/src/app.ts
@@ -1,11 +1,11 @@
-import $ from "jquery";
-
-$(document).ready(() => {
-  $(`.git-svg`).click((e) => {
-    e.preventDefault();
-    console.log(`clicked`);
+(() => {
+  $(document).ready(() => {
+    $(`.git-svg`).click((e) => {
+      e.preventDefault();
+      console.log(`clicked`);
+    });
   });
-});
+})();
 
 // ? set the current dir by "cd siteRecreation/githubProfile"
 // ? to compile run => "tsc -p githubProfile"
