497a6e65a872acbea80c4ce3606310f419e0c8e0
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-02-04 19:52:34 -0600

    creating a save point

--- React/react-basics/src/components/navbar/Navbar.jsx
+++ React/react-basics/src/components/navbar/Navbar.jsx
@@ -3,6 +3,16 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from "../../assets/logo.svg";
 import "./dist/css/navbar.css";
 
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#wgpt3">What is GPT-3</a></p>
+  <p><a href="#possibility">Open AI</a></p>
+  <p><a href="#features">Case Studies</a></p>
+  <p><a href="#blog">Library</a></p>
+  </>
+)
+
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
@@ -13,11 +23,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="navbar-links-container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT-3</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+          <Menu />
         </div>
       </div>
       <div className="navbar-sign">
@@ -28,6 +34,17 @@ const Navbar = () => {
       {toggleMenu
           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+          {toggleMenu && (
+            <div className='navbar-menu-container scale-up-center'>
+              <div className='navbar-menu-container-links'>
+                <Menu />
+                <div className="navbar-menu-container-links-sign">
+                  <p>Sign in</p>
+                  <button type="button">Sign up</button>
+                </div>
+              </div>
+            </div>
+          )}
       </div>
     </div>
   );
